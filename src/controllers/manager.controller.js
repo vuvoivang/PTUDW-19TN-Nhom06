616fe1ac68f0dd263b4cd3e3e67b71dceaa3d272
zenodinh <75799707+zenodinh@users.noreply.github.com>
2022-04-23 22:23:14 +0700

    Change sidebar & navbar

--- src/controllers/manager.controller.js
+++ src/controllers/manager.controller.js
@@ -1,5 +1,50 @@
 module.exports = {
     get: (req, res) => {
         res.render("layouts/manager/main");
+    },
+    // patient
+    getPatientManagement: (req, res) => {
+        res.render("layouts/manager/patientManagement", {
+            layout: "manager/main"
+        });
+    },
+
+    // category
+    getCategoryManagement: (req, res) => {
+        res.render("layouts/manager/categoryManagement", {
+            layout: "manager/main"
+        })
+    },
+    // product
+    getProductManagement: (req, res) => {
+        res.render("layouts/manager/productManagement", {
+            layout: "manager/main"
+        })
+    },
+
+    addProduct: (req, res) => {
+        res.render("layouts/manager/addProduct", {
+            layout: "manager/main"
+        })
+    },
+
+    detailProduct: (req, res) => {
+        res.render("layouts/manager/detailProduct", {
+            layout: "manager/main"
+        })
+    },
+
+    // package
+    getPackageManagement: (req, res) => {
+        res.render("layouts/manager/packageManagement", {
+            layout: "manager/main"
+        })
+    },
+
+    // payment
+    getPaymentManagement: (req, res) => {
+        res.render("layouts/manager/paymentManagement", {
+            layout: "manager/main"
+        })
     }
 }
--- src/routes/manager.route.js
+++ src/routes/manager.route.js
@@ -3,6 +3,23 @@ const router = express.Router();
 
 const managerController = require('../controllers/manager.controller');
 
-router.get('/', managerController.get);
+// patient management
+// router.get("/", managerController.get); // default
+router.get("/", managerController.getProductManagement);    // temporary
+router.get("/patient-management", managerController.getPatientManagement);
+
+// category management
+router.get("/category-management", managerController.getCategoryManagement);
+
+// product management
+router.get("/product-management", managerController.getProductManagement);
+router.get("/product-management/new", managerController.addProduct);
+router.get("/product-management/:id", managerController.detailProduct);
+
+// package management
+router.get("/package-management", managerController.getPackageManagement);
+
+// payment management
+router.get("/payment-management", managerController.getPaymentManagement);
 
 module.exports = router;
