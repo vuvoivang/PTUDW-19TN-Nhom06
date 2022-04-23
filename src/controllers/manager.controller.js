2a0e309502f89e7fcc3d09519683f59a2216d591
DuyOnix <dtduy2210@gmail.com>
2022-04-23 16:31:52 +0700

    handle add product

--- src/controllers/manager.controller.js
+++ src/controllers/manager.controller.js
@@ -2,7 +2,20 @@ module.exports = {
     get: (req, res) => {
         res.render("layouts/manager/main");
     },
+    // patient
+    getPatientManagement: (req, res) => {
+        res.render("layouts/manager/patientManagement", {
+            layout: "manager/main"
+        });
+    },
 
+    // category
+    getCategoryManagement: (req, res) => {
+        res.render("layouts/manager/categoryManagement", {
+            layout: "manager/main"
+        })
+    },
+    // product
     getProductManagement: (req, res) => {
         res.render("layouts/manager/productManagement", {
             layout: "manager/main"
@@ -11,6 +24,20 @@ module.exports = {
 
     createProduct: (req, res) => {
         res.render("layouts/manager/createProduct", {
+            layout: "manager/main"
+        })
+    },
+
+    // package
+    getPackageManagement: (req, res) => {
+        res.render("layouts/manager/packageManagement", {
+            layout: "manager/main"
+        })
+    },
+
+    // payment
+    getPaymentManagement: (req, res) => {
+        res.render("layouts/manager/paymentManagement", {
             layout: "manager/main"
         })
     }
--- src/routes/manager.route.js
+++ src/routes/manager.route.js
@@ -6,16 +6,19 @@ const managerController = require('../controllers/manager.controller');
 // patient management
 // router.get("/", managerController.get); // default
 router.get("/", managerController.getProductManagement);    // temporary
-
+router.get("/patient-management", managerController.getPatientManagement);
 
 // category management
+router.get("/category-management", managerController.getCategoryManagement);
 
 // product management
 router.get("/product-management", managerController.getProductManagement);
-router.get("/product-management/create", managerController.createProduct);
+router.get("/product-management/new", managerController.createProduct);
 
 // package management
+router.get("/package-management", managerController.getPackageManagement);
 
 // payment management
+router.get("/payment-management", managerController.getPaymentManagement);
 
 module.exports = router;
