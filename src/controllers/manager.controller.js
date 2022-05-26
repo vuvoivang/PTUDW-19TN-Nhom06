a2cd237d939f63d1143a5ecf03b188c8bca8bd11
DuyOnix <dtduy2210@gmail.com>
2022-05-27 00:33:47 +0700

    layout for category management

--- src/controllers/manager.controller.js
+++ src/controllers/manager.controller.js
@@ -1,49 +1,55 @@
+const { categories, products } = require('../models/manager.model');
+const path = "layouts/manager";
+
+
 module.exports = {
     get: (req, res) => {
-        res.render("layouts/manager/main");
+        res.render(`${path}/main`);
     },
     // patient
     getPatientManagement: (req, res) => {
-        res.render("layouts/manager/patientManagement", {
+        res.render(`${path}/patientManagement`, {
             layout: "manager/main"
         });
     },
 
     // category
     getCategoryManagement: (req, res) => {
-        res.render("layouts/manager/categoryManagement", {
-            layout: "manager/main"
+        res.render(`${path}/categoryManagement`, {
+            layout: "manager/main",
+            categories
         })
     },
     // product
     getProductManagement: (req, res) => {
-        res.render("layouts/manager/productManagement", {
-            layout: "manager/main"
+        res.render(`${path}/productManagement`, {
+            layout: "manager/main",
+            products
         })
     },
 
     addProduct: (req, res) => {
-        res.render("layouts/manager/addProduct", {
+        res.render(`${path}/addProduct`, {
             layout: "manager/main"
         })
     },
 
     detailProduct: (req, res) => {
-        res.render("layouts/manager/detailProduct", {
+        res.render(`${path}/detailProduct`, {
             layout: "manager/main"
         })
     },
 
     // package
     getPackageManagement: (req, res) => {
-        res.render("layouts/manager/packageManagement", {
+        res.render(`${path}/packageManagement`, {
             layout: "manager/main"
         })
     },
 
     // payment
     getPaymentManagement: (req, res) => {
-        res.render("layouts/manager/paymentManagement", {
+        res.render(`${path}/paymentManagement`, {
             layout: "manager/main"
         })
     }
