387c62642a5f259cdecaf01adec56118531379f3
DuyOnix <dtduy2210@gmail.com>
2022-06-03 11:17:16 +0700

    handle role product package

---
+++ src/models/Order.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { ObjectId } = mongoose.Schema.Types;
+
+const orderSchema = new Schema({
+    user: {
+        type: ObjectId,
+        ref: 'Account',
+        required: true
+    },
+    item: {
+        type: ObjectId, // can be ref to Product or Package model base on type
+        refPath: "type",
+    },
+    type: {
+        type: String,
+        enum: ['Product', 'Package'],
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    orderTime: {
+        type: Date,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['cash', 'credit-card'],
+        required: true
+    },
+    paymentAccount: {
+        type: String,
+        ref: "Payment_Account",   
+        required: true
+    },
+    paymentTime: {
+        type: Date,
+        required: true
+    },
+    deliveryAddress: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'processing', 'delivering', 'delivered', 'cancelled'],
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Order', orderSchema);
