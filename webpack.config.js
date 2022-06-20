cdcaa291a7a9865cd67073177d39f723d53c2541
zenodinh <75799707+zenodinh@users.noreply.github.com>
2022-06-20 19:09:32 +0700

    Fix admin create page

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
     entry: {
         signup: path.resolve(__dirname, 'src/public/js/view/signup.js'),
         signin: path.resolve(__dirname, 'src/public/js/view/login.js'),
-        signout: path.resolve(__dirname, 'src/utils/sidebarFunc.js')
     },
     output: {
         path: path.resolve(__dirname, 'src/public/js/authentication'), 
